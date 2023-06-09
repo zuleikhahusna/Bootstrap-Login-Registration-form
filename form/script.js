d481072395db4fe1f5a98bfd7c16e0acc4d3bc4e
zuleikhahusna <135536049+zuleikhahusna@users.noreply.github.com>
2023-06-09 10:51:16 +0100

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   const createBtn = document.querySelector(".btn");
+//   const form = document.querySelector("form");
+
+//   createBtn.addEventListener("mouseover", function() {
+//     if (!form.checkValidity()) {
+//       createBtn.classList.add("incomplete");
+//     }
+//   });
+
+//   createBtn.addEventListener("mouseout", function() {
+//     createBtn.classList.remove("incomplete");
+//   });
+// });
